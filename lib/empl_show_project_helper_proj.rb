55bce1a78fee5bf1ada2c34f783f1ae027998a7f
ryanden2018 <denlinger.ryan@gmail.com>
2019-04-16 22:10:50 -0500

    optimizing activerecord queries 2

--- lib/empl_show_project_helper_proj.rb
+++ lib/empl_show_project_helper_proj.rb
@@ -1,5 +1,5 @@
 def empl_show_project_helper_proj(prompt,employee,project)
-  tasks = employee.tasks.select { |task| task.project_id == project.id }.uniq.sort_by do |task|
+  tasks = employee.tasks.where(project_id: project.id).uniq.sort_by do |task|
     if task.due_date
       yyyy,mm,dd = task.due_date.split(" ")[0].split("-").map { |x| x.to_i }
       Date.new(yyyy,mm,dd).to_time.to_i
--- lib/empl_show_project_helper_spec_empl.rb
+++ lib/empl_show_project_helper_spec_empl.rb
@@ -1,7 +1,7 @@
 def empl_show_project_helper_spec_empl(prompt,employee,project)
   cd_reset = "\e[0m"
   page_str = "\n" + cd_reset + "Other employees with your specialty\n"
-  page_str +=  cd_reset + "-----------------------------------\n"\
+  page_str +=  cd_reset + "-----------------------------------\n"
   Employee.where(specialty_id: employee.specialty_id).each do |other_employee|
     if other_employee != employee
       page_str += cd_reset + other_employee.first_name + " " + other_employee.last_name + "\n"
