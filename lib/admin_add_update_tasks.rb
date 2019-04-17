106c01b56b7c8b7726ddb53c0289c700257f238f
kreestoff <christopher.l.mcintosh@outlook.com>
2019-04-17 11:32:46 -0500

    added functionality to select available specialties when creating new employee or editing an employee

---
+++ lib/admin_add_update_tasks.rb
@@ -0,0 +1,26 @@
+def admin_add_update_tasks(prompt, project)
+  task_list = Task.all.select {|task| task.project_id == project.id}
+  sel_new_task = -1
+  sel_return = -2
+
+  puts "\e[H\e[2J"
+  puts "Project: #{project.name}"
+  selection = prompt.select("Select Task to Edit, Add task or Return.") do |menu|
+    task_list.each do |task|
+      menu.choice "ID: #{task.id} Description: #{task.description} Employee ID: #{task.employee_id} Completed: #{task.completed} Due Date: #{task.due_date}"
+    end
+    menu.choice "Create new task for #{project.name} project.", sel_new_task
+    menu.choice "Return", sel_return
+  end
+
+  case selection
+  when sel_return
+  when sel_new_task
+    admin_new_task(prompt, project)
+  else
+    admin_edit_task(prompt, Task.find(selection))
+end
+
+
+
+end
